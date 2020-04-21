4dd39812e48f9b9e0adeda2f7e658d256304a137
Rudolph <rudolph.nativel@gmail.com>
2020-04-21 23:37:42 +0200

    tp1 module6

---
+++ tp1 module6/Reference Module 06 TP 01/Arme.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BO
+{
+    public class Arme
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30,MinimumLength =3)]
+        public string Nom { get; set; }
+        public int Degats { get; set; }
+    }
+}
---
+++ tp1 module6/Reference Module 06 TP 01/Samourai.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BO
+{
+    public class Samourai
+    {
+        public int Id { get; set; }
+        [Required]
+        public int Force { get; set; }
+        [Required]
+        [StringLength(25, MinimumLength = 5)]
+        public string Nom { get; set; }
+        public virtual Arme Arme { get; set; }
+    }
+}
